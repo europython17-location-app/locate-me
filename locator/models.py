8402ebe1a4dabd2a75895eb93dae439f86a5b021
Irmantas Ramoška <i@ramoska.eu>
2017-07-09 15:36:02 +0200

    [VERY NOT WORKING] Add user and location models

--- locator/models.py
+++ locator/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_model
 
 # Create your models here.
 
+class User(models.Model):
+    uuid = models.UUIDField(primary_key=True)
+
+class UserLocation(gis_model.Model):
+    user = models.ForeignObject(to=User, on_delete=None, from_fields=[])
+    coords = gis_model.PointField()
